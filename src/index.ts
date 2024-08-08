749e116c658b3426db5dbf55eea8de204a2a1f36
aakoch <github2@adamkoch.com>
2024-08-07 21:24:50 -0500

    saving progress
    Signed-off-by: aakoch <github2@adamkoch.com>

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
-import { parseArguments as originalParseArguments, exists, isSupportedFileExtension, simpleProjectRootDir, withCreateStreams } from '@foo-dog/utils';
-
-
-declare type Options = {
-  optional: [],
-  required: []
+declare module 'utils' {
+  export type Options = {
+    optional: [],
+    required: []
+  }
+  export type Argv = string[];
+  export type ReturnOptions = {
+    [key: string]: string
+  }
+  export type Ret = {
+    args?: string[],
+    options?: ReturnOptions,
+    in?: any,
+    out?: any
+  }
+  export function isSupportedFileExtension(extension: string): boolean;
+  export function simpleProjectRootDir(): string;
+  export function exists(path: string): boolean;
+  export function parseArguments(processOrArgv: NodeJS.Process | Argv, printUsage?: Function, options?: Options): Promise<Ret>;
+  export function isSupportedFileExtension(extension: string): boolean;
+  export function simpleProjectRootDir(): string;
+  export function exists(path: string): boolean;
 }
-
-declare type Argv = string[];
-
-declare type ReturnOptions = {
-  [key: string]: string
-}
-
-declare type Ret = {
-  args?: string[],
-  options?: ReturnOptions,
-  in?: any,
-  out?: any
-}
-
-async function parseArguments(processOrArgv: NodeJS.Process | Argv, printUsage?: Function, options?: Options): Promise<Ret> {
-  return originalParseArguments(processOrArgv, printUsage, options)
-}
-
-export { parseArguments, exists, isSupportedFileExtension, simpleProjectRootDir, withCreateStreams };
